9f115925216e20de7f6a697fc384d2b4bac71ee4
Sam G. Zun <zunsamuel97@gmail.com>
2018-07-26 02:37:45 -0500

    on click random images

--- click-game/src/containers/Clickables/Clickables.js
+++ click-game/src/containers/Clickables/Clickables.js
@@ -1,32 +1,47 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import "./Clickables.css";
-import Character from "../../components/Character/Character";
+import './Clickables.css';
+import Character from '../../components/Character/Character';
 
-import Tammy from "../../assets/media/tammy.png";
-import Beth from "../../assets/media/beth.jpg";
-import BirdPerson from "../../assets/media/bird_person.jpeg";
-import Rick from "../../assets/media/rick.jpg";
-import Jerry from "../../assets/media/jerry.jpg";
-import MrPB from "../../assets/media/mr_pb.png";
-import MeeSeeks from "../../assets/media/mr_me_seeks.png";
-import MailMan from "../../assets/media/mail_man.jpg";
-import KrombopulusMicheal from "../../assets/media/krombopulos_micheal.png";
-import Summer from "../../assets/media/summer.png";
-import Squanchy from "../../assets/media/squanchy.png";
-import Goldenfold from "../../assets/media/goldenfold.jpg";
+import Tammy from '../../assets/media/tammy.png';
+import Beth from '../../assets/media/beth.jpg';
+import BirdPerson from '../../assets/media/bird_person.jpeg';
+import Rick from '../../assets/media/rick.jpg';
+import Jerry from '../../assets/media/jerry.jpg';
+import MrPB from '../../assets/media/mr_pb.png';
+import MeeSeeks from '../../assets/media/mr_me_seeks.png';
+import MailMan from '../../assets/media/mail_man.jpg';
+import KrombopulusMicheal from '../../assets/media/krombopulos_micheal.png';
+import Summer from '../../assets/media/summer.png';
+import Squanchy from '../../assets/media/squanchy.png';
+import Goldenfold from '../../assets/media/goldenfold.jpg';
 
 class Clickables extends Component {
   state = {
-    hasBeenClicked: false
+    hasBeenClicked: false,
+    characters: [
+      Tammy,
+      Beth,
+      BirdPerson,
+      Rick,
+      Jerry,
+      MrPB,
+      MeeSeeks,
+      MailMan,
+      KrombopulusMicheal,
+      Summer,
+      Squanchy,
+      Goldenfold
+    ]
   };
 
   randomImg = masterList => {
     const masterImages = [];
+    const banan = [...masterList];
     for (var i = 0; i < 12; i++) {
-      let figure = masterList[Math.floor(Math.random() * masterList.length)];
-      const index = masterList.indexOf(figure);
-      masterList.splice(index, 1);
+      let figure = banan[Math.floor(Math.random() * banan.length)];
+      const index = banan.indexOf(figure);
+      banan.splice(index, 1);
       masterImages.push(figure);
       if (i === 11) {
         return masterImages;
@@ -50,12 +65,16 @@ class Clickables extends Component {
   ];
 
   character = () =>
-    this.randomImg(this.masterList).map(headshot => (
-      <Character image={headshot} clicked={() => this.character()} />
-    ));
+    this.setState({ characters: this.randomImg(this.masterList) });
 
   render() {
-    return <div className="Clickables">{this.character()}</div>;
+    return (
+      <div className="Clickables">
+        {this.state.characters.map(headshot => (
+          <Character image={headshot} clicked={() => this.character()} />
+        ))}
+      </div>
+    );
   }
 }
 
